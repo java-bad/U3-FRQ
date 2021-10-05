226ce7842b94dda596711376a09bd8c02f9f287e
java-bad <91962179+java-bad@users.noreply.github.com>
2021-10-05 08:44:29 -0500

    a

--- SpinnerGame.java
+++ SpinnerGame.java
@@ -10,9 +10,24 @@ public class SpinnerGame
 
 */
 
+
+/** Precondition: min < max
+
+* Simulates a spin of a spinner by returning a random integer
+
+* between min and max, inclusive.
+
+*/
+
 public int spin(int min, int max)
 
-{ /* to be implemented in part (a) */ }
+{
+
+int result = (int)(Math.floor((Math.random() * (max - min)) + min));
+
+  return result;
+
+}
 
  
 
