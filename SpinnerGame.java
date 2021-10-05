341ce84df20b593a1ae6f1e0fd22b64754e9d0a1
java-bad <91962179+java-bad@users.noreply.github.com>
2021-10-05 08:37:22 -0500

    Create SpinnerGame.java

---
+++ SpinnerGame.java
@@ -0,0 +1,27 @@
+public class SpinnerGame
+
+{
+
+/** Precondition: min < max
+
+* Simulates a spin of a spinner by returning a random integer
+
+* between min and max, inclusive.
+
+*/
+
+public int spin(int min, int max)
+
+{ /* to be implemented in part (a) */ }
+
+ 
+
+/** Simulates one round of the game as described in part (b).
+
+*/
+
+public void playRound()
+
+{ /* to be implemented in part (b) */ }
+
+}
